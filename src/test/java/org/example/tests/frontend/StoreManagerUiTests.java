39de6f2301442a9edc79d8cb99e22d86327dc15a
ValCat94 <valeri.marquise.des.anges@gmail.com>
2025-10-07 01:49:18 +0700

    useUser and create/quit WebDriver

--- src/test/java/org/example/tests/frontend/StoreManagerUiTests.java
+++ src/test/java/org/example/tests/frontend/StoreManagerUiTests.java
@@ -1,7 +1,10 @@
 package org.example.tests.frontend;
 
 import org.example.tests.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StoreManagerUiTests extends BaseTest {
 
@@ -9,8 +12,23 @@ public class StoreManagerUiTests extends BaseTest {
     http://localhost:8000
      */
 
+    String email;
+    String password;
+
+    @BeforeEach
+    void useUser() {
+        email = "v@v.ru";
+        password = "123456";
+    }
+
     @Test
     void loginTest() {
+        WebDriver driver = new ChromeDriver();
 
+        /*
+
+         */
+
+        driver.quit();
     }
 }
