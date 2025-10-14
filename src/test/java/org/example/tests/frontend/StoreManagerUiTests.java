a18f86020d85b553b32bce852ae5536186252594
ValCat94 <valeri.marquise.des.anges@gmail.com>
2025-10-15 01:55:03 +0700

    Use User model

--- src/test/java/org/example/tests/frontend/StoreManagerUiTests.java
+++ src/test/java/org/example/tests/frontend/StoreManagerUiTests.java
@@ -15,16 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class StoreManagerUiTests extends BaseTest {
 
     User testUser;
-    String email;
-    String password;
 
     @BeforeEach
     void useUser() {
         testUser = useTestUser();
-    }
-
-    private User useTestUser() {
-        return User.builder().email("v@v.ru").password("12345678").build();
     }
 
     @Test
@@ -35,12 +29,16 @@ public class StoreManagerUiTests extends BaseTest {
 
         driver.get(APP_UI_URL);
 
-        driver.findElement(By.cssSelector("#email")).sendKeys(email);
-        driver.findElement(By.cssSelector("#password")).sendKeys(password);
+        driver.findElement(By.cssSelector("#email")).sendKeys(testUser.getEmail());
+        driver.findElement(By.cssSelector("#password")).sendKeys(testUser.getPassword());
         driver.findElement(By.cssSelector("button.btn")).click();
 
         assertTrue(driver.findElement(By.xpath("//a[contains(.,'Личный кабинет')]")).isDisplayed());
 
         driver.quit();
+    }
+
+    private User useTestUser() {
+        return User.builder().email("v@v.ru").password("12345678").build();
     }
 }
