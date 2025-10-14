ffdfbd95ae311b5fb534e4da15e1fab35b6e1696
ValCat94 <valeri.marquise.des.anges@gmail.com>
2025-10-15 02:22:18 +0700

    setupData and tearDown

--- src/test/java/org/example/tests/frontend/StoreManagerUiTests.java
+++ src/test/java/org/example/tests/frontend/StoreManagerUiTests.java
@@ -2,6 +2,7 @@ package org.example.tests.frontend;
 
 import org.example.frontend.models.User;
 import org.example.tests.BaseTest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -15,27 +16,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class StoreManagerUiTests extends BaseTest {
 
     User testUser;
+    WebDriver driver;
 
     @BeforeEach
-    void useUser() {
+    void setupData() {
         testUser = useTestUser();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(APP_UI_URL);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
     }
 
     @Test
     void loginTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        driver.get(APP_UI_URL);
-
         driver.findElement(By.cssSelector("#email")).sendKeys(testUser.getEmail());
         driver.findElement(By.cssSelector("#password")).sendKeys(testUser.getPassword());
         driver.findElement(By.cssSelector("button.btn")).click();
 
         assertTrue(driver.findElement(By.xpath("//a[contains(.,'Личный кабинет')]")).isDisplayed());
-
-        driver.quit();
     }
 
     private User useTestUser() {
