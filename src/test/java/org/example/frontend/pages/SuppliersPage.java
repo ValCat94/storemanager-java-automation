b13c71460b011ff6da18f376901e034b26d08add
ValCat94 <valeri.marquise.des.anges@gmail.com>
2025-10-18 23:58:41 +0700

    implement NavBar to Suppliers and Warehouses

--- src/test/java/org/example/frontend/pages/SuppliersPage.java
+++ src/test/java/org/example/frontend/pages/SuppliersPage.java
@@ -1,22 +1,15 @@
 package org.example.frontend.pages;
 
+import lombok.Data;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class SuppliersPage {
+@Data
+public class SuppliersPage implements HasNavigationBar {
     WebDriver driver;
-
-    @FindBy(xpath = "//a[contains(.,'Личный кабинет')]")
-    WebElement personalAccount;
 
     public SuppliersPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
-    }
-
-    public boolean isPersonalAccountDisplayed() {
-        return personalAccount.isDisplayed();
     }
 }
--- src/test/java/org/example/frontend/pages/WarehousesPage.java
+++ src/test/java/org/example/frontend/pages/WarehousesPage.java
@@ -1,22 +1,15 @@
 package org.example.frontend.pages;
 
+import lombok.Data;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class WarehousesPage {
+@Data
+public class WarehousesPage implements HasNavigationBar {
     WebDriver driver;
-
-    @FindBy(xpath = "//a[contains(.,'Личный кабинет')]")
-    WebElement personalAccount;
 
     public WarehousesPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
-    }
-
-    public boolean isPersonalAccountDisplayed() {
-        return personalAccount.isDisplayed();
     }
 }
