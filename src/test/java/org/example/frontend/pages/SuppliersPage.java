60595c6ab092bf10633ff473206cce50dde7b883
ValCat94 <valeri.marquise.des.anges@gmail.com>
2025-10-19 03:14:52 +0700

    get table row by name

--- src/test/java/org/example/frontend/pages/SuppliersPage.java
+++ src/test/java/org/example/frontend/pages/SuppliersPage.java
@@ -1,15 +1,30 @@
 package org.example.frontend.pages;
 
 import lombok.Data;
+import org.example.frontend.pages.elements.SuppliersTableRow;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 @Data
 public class SuppliersPage implements HasNavigationBar {
     WebDriver driver;
 
+    @FindBy(css = "table#suppliers-table tbody tr")
+    List<WebElement> suppliersTableRows;
+
     public SuppliersPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
+    }
+
+    public SuppliersTableRow getTableRowByName(String name) {
+        return suppliersTableRows.stream()
+                .map(SuppliersTableRow::new)
+                .filter(row -> row.getName().equals(name))
+                .findFirst().orElseThrow();
     }
 }
