70ccfeba94c849d9c1d5fa73b323feb7e83014e6
Glinskaya.V.L <Glinskaya.V.L@sbertech.ru>
2025-12-12 23:08:04 +0700

    Add RegisterResponse model

--- src/test/java/org/example/backend/models/RegisterResponse.java
+++ src/test/java/org/example/backend/models/RegisterResponse.java
@@ -1,8 +1,16 @@
 package org.example.backend.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 // Аннотации Lombok для автоматической генерации геттеров, сеттеров, конструкторов и других методов
 public class RegisterResponse {
-
     // Поля класса, представляющие данные для ответа об успешной регистрации пользователя
+    String message;
+    String name;
 
 }
